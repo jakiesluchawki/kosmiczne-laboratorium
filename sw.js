e91f2c5a91946c19c699c801942ceee6e3750c41
jakiesluchawki <mieszko@mahboob.pl>
2026-06-14 20:10:43 +0200

    deploy: publish verified application

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kosmiczne-laboratorium-02ab2c2310455dfa";
+const CACHE = "kosmiczne-laboratorium-d1724826e67fad78";
 const OFFLINE_URL = "./index.html";
 const PROTECT_PRIVATE_ROUTES = true;
 const PRIVATE_SERVICE_WORKER_PREFIXES = ["review","__voice-review"];
