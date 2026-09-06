4a1626954101cab9d7af435597e1796c4500462f
jakiesluchawki <mieszko@mahboob.pl>
2026-09-06 13:48:52 +0200

    deploy: publish verified application

--- assets/index-Bbrqg-e7.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-DsAOOook.js","./index-CNHTL47a.js"])))=>i.map(i=>d[i]);
-import{_ as r}from"./index-B85lpD66.js";import{registerPlugin as t}from"./index-CNHTL47a.js";const o=t("ScreenOrientation",{web:()=>r(()=>import("./web-DsAOOook.js"),__vite__mapDeps([0,1]),import.meta.url).then(e=>new e.ScreenOrientationWeb)});export{o as ScreenOrientation};
